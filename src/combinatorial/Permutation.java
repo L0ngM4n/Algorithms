fbd0f582b8afdafe80f32de7e828e53c8168a453
L0ngM4n <aleksandar.stanchev@gmail.com>
2017-07-27 17:47:16 +0300

    Undo some deletes

---
+++ src/combinatorial/Permutation.java
@@ -0,0 +1,32 @@
+package combinatorial;
+
+import tools.Helpers;
+
+import java.util.HashSet;
+
+/**
+ * 27/07/2017
+ */
+public class Permutation {
+
+  public static <T> void gen(T[] elements, int index){
+
+    if (index >= elements.length) {
+      Helpers.print(elements);
+    } else {
+      gen(elements, index + 1);
+
+      HashSet<T> used = new HashSet<>();
+      used.add(elements[index]);
+      for (int i = index  + 1; i < elements.length; i++) {
+        if (!used.contains(elements[i])){
+          used.add(elements[i]);
+
+          Helpers.swap(elements, index, i);
+          gen(elements, index + 1);
+          Helpers.swap(elements, index, i);
+        }
+      }
+    }
+  }
+}
---
+++ src/combinatorial/Variation.java
@@ -0,0 +1,32 @@
+package combinatorial;
+
+/**
+ * 27/07/2017
+ */
+public class Variation {
+  public static <T> void gen(T[] elements, int k) {
+
+    int[] vector = new int[k];
+
+    while (true) {
+
+      printVar(elements, vector);
+
+      int index = k - 1;
+
+      while (index >= 0 && vector[index] == elements.length - 1) {
+        index--;
+      }
+    }
+
+  }
+
+  private static <T> void printVar(T[] elements, int[] vector) {
+
+    for (int i = 0; i < vector.length; i++) {
+      System.out.println(elements[vector[i]]);
+    }
+    System.out.println();
+  }
+
+}
